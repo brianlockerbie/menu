28aa38de72d5a5e561875face1069deb08f41a13
Brian Lockerbie <brian.lockerbie@gmail.com>
2021-04-14 13:30:31 -0700

    Category buttons work

--- app.js
+++ app.js
@@ -84,7 +84,19 @@ window.addEventListener("DOMContentLoaded", function () {
 // filter items
 filterBtns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
-    console.log(e.currentTarget.dataset.id);
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      // console.log(menuItem.category);
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    // console.log(menuCategory);
+    if (category === 'all') {
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
   });
 });
 
