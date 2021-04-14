e132fa1e9ccb9ef691953178eca24cc019f611c4
Brian Lockerbie <brian.lockerbie@gmail.com>
2021-04-14 14:02:14 -0700

    Added buttons dynamically

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// get only unique categories - HARDEST ONE
+// iterate over categories return buttons
+// make sure to select buttons when they are available
+
 // items
 const menu = [
   {
@@ -72,15 +76,40 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns =document.querySelectorAll(".filter-btn");
-
+const container = document.querySelector(".btn-container");
 // load items
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItems(menu);
-});
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" type="button"
+      data-id=${category}>
+      ${category}
+      </button>`;
+    })
+    .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
 // filter items
 filterBtns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
@@ -98,6 +127,7 @@ filterBtns.forEach(function (btn) {
       displayMenuItems(menuCategory);
     }
   });
+});
 });
 
 function displayMenuItems(menuItems) {
